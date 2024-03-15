8d187c6b8463499e18f75c883189738eeb47b233
Richard <richard_dong50@hotmail.com>
2024-03-15 11:50:06 -0700

    working cue split

--- CueSplit.py
+++ CueSplit.py
@@ -1,11 +1,13 @@
 import os
-import time
 import re
-import File_Renaming as FR
+import utilities as util
+import File_Renaming as fr
+import sys
+from mutagen.flac import Picture
+from mutagen.flac import FLAC
 from ffcuesplitter.cuesplitter import FFCueSplitter
+from ffcuesplitter.user_service import FileSystemOperations
 
-def SubstringAfter(s: str , delim: str):
-    return s.partition(delim)[2]
 
 class TrackData:
     Title = ""
@@ -40,15 +42,13 @@ class CueFile:
 
     def __init__(self, path) -> None:
         srcPath = path
-
         f = open(srcPath, "r", encoding="utf-8", errors="replace")
-        
         line = f.readline()
-        # parse the infromation
-        self.Genre = SubstringAfter(line, "REM GENRE").lstrip().rstrip()
-        
+
+        # parse the cue file for metadata
+        self.Genre = util.SubstringAfter(line, "REM GENRE").lstrip().rstrip()
         line = f.readline()
-        self.Date = SubstringAfter(line, "REM Date").lstrip().rstrip()
+        self.Date = util.SubstringAfter(line, "REM DATE").lstrip().rstrip()
 
         # get the Performer
         line = f.readline()
@@ -73,22 +73,25 @@ class CueFile:
             index00 = ""
             index01 = ""
             if ("INDEX 00" in line):
-                index00 = SubstringAfter(line, "INDEX 00").lstrip().rstrip()
+                index00 = util.SubstringAfter(line, "INDEX 00").lstrip().rstrip()
                 line = f.readline()
                 if ("INDEX 01" in line):
-                    index01 = SubstringAfter(line, "INDEX 01").lstrip().rstrip()  
+                    index01 = util.SubstringAfter(line, "INDEX 01").lstrip().rstrip()  
             elif ("INDEX 01" in line):
-                index01 = SubstringAfter(line, "INDEX 01").lstrip().rstrip()
+                index01 = util.SubstringAfter(line, "INDEX 01").lstrip().rstrip()
             
             trackData = TrackData(trackTitle, trackPerformer, index00, index01)
             self.TrackList.append(trackData)
             line = f.readline()
-        pass
-
 
     # create a new .cue file for 
     def writeToCueFile(self, path) -> None:
-        f = open(path, "w+")
+        """
+        Write CueFile data to file pointed by path
+        Input:
+            path (str): target .cue file path
+        """
+        f = open(path, "w+", encoding="utf-8")
 
         f.write(f"REM GENRE {self.Genre}\n")
         f.write(f"REM DATE {self.Date}\n")
@@ -109,6 +112,13 @@ class CueFile:
 
     
     def setTrackData(self, id, title = "", performer = ""):
+        """
+        Set track information
+        Input:
+            id (int): track id we want to change
+            title (str): new title we want to set for track
+            performer(str): new performer we want to set for track
+        """
         id = id - 1
         if title != "":
             self.TrackList[id].setTitle(title)
@@ -117,23 +127,26 @@ class CueFile:
             self.TrackList[id].setPerformer(performer)
 
 
+# TODO:: include batch processing
 if __name__ == "__main__":
+
     cwd = os.getcwd()
+    targetDir = os.path.join(cwd, sys.argv[1])
+    dirName = sys.argv[1].rstrip(" \"/\\").lstrip(".\\/")
 
-    targetDir = os.path.join(cwd,"群星《2024试听小屋系列(115)》[FLAC]")
-    targetFile = os.path.join(targetDir,"群星《2024试听小屋系列(115)》[FLAC].cue")
+    # check if targetDir is legitimate
+    assert os.path.exists(targetDir) == True
 
-    targetTXTFile = os.path.join(targetDir, "群星《2024试听小屋系列(115)》[FLAC].txt")
-
+    musicDir = fr.MusicDir(targetDir, dirName=dirName)
     cueCopy = os.path.join(targetDir, "test_COPY.cue")
-    
-    cueFile = CueFile(targetFile)
-    toc = FR.TableOfContents(targetTXTFile)
 
-    # # update cuefile metadata with what was obtained in TableOfContents
+    cueFile = CueFile(musicDir.cuePath)
+    toc = fr.TableOfContents(musicDir.tableOfContentsPath)
+
+    # update cuefile metadata with what was obtained in TableOfContents
 
     cueFile.AlbumTitle = toc.albumName
-    cueFile.FileName = "群星《2024试听小屋系列(115)》[FLAC].flac"
+    cueFile.FileName = musicDir.dirName + ".flac"
     cueFile.Performer = toc.albumArtist
 
     # update the track data
@@ -144,6 +157,28 @@ if __name__ == "__main__":
 
     cueFile.writeToCueFile(cueCopy)
 
-    getData = FFCueSplitter(filename=cueCopy, dry=True)
-    # print(getData.audiotracks())
+    # set the cover picture for the major flac file
+    try:
+        targetFlacFile = fr.FLACDataDef(musicDir.majorFlacPath, musicDir.coverPath)
+        targetFlacFile.updateMetaData()
+
+    except AssertionError as msg:
+        print(msg)
+
+
+
+    # obtain data of flac file
+    # getData = FFCueSplitter(filename=cueCopy, outputdir=targetDir, outputformat="flac", dry=True)
+    # l = getData.audiotracks
+    # print(l)
+
+    # split the data
+    # split = FileSystemOperations(filename=cueCopy, outputdir=targetDir, outputformat="flac", dry=False)
+    # if split.kwargs['dry']:
+    #     split.dry_run_mode()
+    # else:
+    #     overwr = split.check_for_overwriting()
+    #     if not overwr:
+    #         split.work_on_temporary_directory()
+ 
     
